37987cbbb795564c0948a685c7074b9b550b40d9
Jan Christlieb <mail@janchristlieb.de>
2018-11-29 12:59:57 +0100

    added nova to deployment script

--- build/fabfile.py
+++ build/fabfile.py
@@ -117,6 +117,7 @@ def _deploy_git_factory():
 
       #fab.execute(css)
       for filename in (
+        'nova',
         'vendor',
       ):
         if os.path.exists(os.path.join(self.repo.working_tree_dir, filename)):
